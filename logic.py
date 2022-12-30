6dbd7bc9a9491329732fb992cfd13bcd16390056
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2022-12-30 21:19:27 +0100

    działa na 3

--- logic.py
+++ logic.py
@@ -28,7 +28,7 @@ pattern2 = [
   [None, 'B', None, 'B', None, 'B', None, 'B', 'A', 'B', None, 'B', None],
   ['A', None, 'A', None, 'A', None, 'A', 'A', 'A', None, 'A', None, 'A'],
   [None, 'B', None, 'B', None, 'B', 'A', 'B', None, 'B', None, 'B', None],
-  ['A', None, 'A', None, 'A', None, 'A', 'A', 'A', 'A', 'A', None, 'A'],
+  ['A', None, 'A', None, 'A', None, 'A', 'A', 'A', 'A', 'A', 'A', 'A'],
   [None, 'B', None, 'B', None, 'B', None, 'B', None, 'B', None, 'B', None],
 ]
 class Game:
@@ -68,31 +68,41 @@ def check_connection(pattern, player, cords=None):
 
         if j == 0 and len(next_checks) == 0:
             i += 2
+        elif j == 0 and len(next_checks) == 1:
+            checked.append((i, j))
+            i, j = next_checks[0]
+        elif j != 0 and len(next_checks) == 1:
+            return None
 
-
-
-
-        # elif j == 0 and len(next_checks) == 1:
+        # elif j != 0:
         #     checked.append((i, j))
-        #     i, j = next_checks[0]
-        # elif j != 0 and len(next_checks) == 2:
-        #     checked.append((i, j))
-        #     if next_checks[0] not in checked:
-        #         i, j = next_checks[0]
-        #     elif next_checks[1] not in checked:
-        #         i, j = next_checks[1]
-        #     else:
-        #         return None
-
-        # elif j != 0 and len(next_checks) == 3:
-        #     checked.append((i, j))
-        #     for x in range(3):
+        #     for x in range(len(next_checks)):
         #         if next_checks[x] not in checked:
         #             if check_connection(pattern, player, next_checks[x]) is not None:
         #                 return player
         #             else:
         #                 checked.append(next_checks[x])
         #     return None
+
+
+        elif j != 0 and len(next_checks) == 2:
+            checked.append((i, j))
+            if next_checks[0] not in checked:
+                i, j = next_checks[0]
+            elif next_checks[1] not in checked:
+                i, j = next_checks[1]
+            else:
+                return None
+
+        elif j != 0 and len(next_checks) == 3:
+            checked.append((i, j))
+            for x in range(3):
+                if next_checks[x] not in checked:
+                    if check_connection(pattern, player, next_checks[x]) is not None:
+                        return player
+                    else:
+                        checked.append(next_checks[x])
+            return None
 
 
             # checked.append((i, j))
@@ -113,11 +123,12 @@ def check_connection(pattern, player, cords=None):
             #         checked.append(next_checks[2])
             # else:
             #     return None
-            # # return "niedokończony pomysł"
+            # return "niedokończony pomysł"
 
         elif j != 0 and len(next_checks) == 4:
             pass
         else:
+            print("coś nie działa")
             return None
 
         if j == 11:
