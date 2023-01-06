88ad506d17cfcfa67fdd07d993379ddc5af40868
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-06 14:39:10 +0100

    Poprawiono sprawdzanie siatki gry dla gracza B

--- logic.py
+++ logic.py
@@ -1,45 +1,52 @@
 from grid import grid, gridA, gridB
 
 class Game:
-    def __init__(self):
-        self._grid = gridB
-        # self.playerA = "A"
-        # self.playerB = "B"
+    def __init__(self, game_grid=None):
+        self._grid = grid if game_grid is None else game_grid
 
     def get_tile(self, point: tuple[int, int]):
         return self._grid[point[0]][point[1]]
+        # return self._check_grid[point[0]][point[1]]
 
     def change_tile(self, point: tuple[int, int], player: str):
         x, y = point
         self._grid[x][y] = player
 
-    def check_near_connection(self, point: tuple[int, int], player: str) -> list:
+    def check_near_connection(self,
+                              point: tuple[int, int],
+                              player: str,
+                              check_grid=None) -> list:
+
         i, j = point
+        check_grid = self._grid[:] if check_grid is None else check_grid
         check_list = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
         neighbors = [(i + x, j + y) for x, y in check_list
                     if (0 <= i + x <= 12 and 0 <= j + y <= 12 and
-                    self._grid[i + x][j + y] == player)]
+                    check_grid[i + x][j + y] == player)]
         return neighbors
 
     def check_win_connection(self,
                              player: str,
+                             check_grid=None,
                              cords=None,
                              checked=None):
+
+        check_grid = self._grid[:] if check_grid is None else check_grid
+        checked = checked if checked is not None else []
+
         if cords == None:
             i = 1
             j = 0
             if player == 'B':
-                rotated = list(zip(*self._grid))[::-1]
+                rotated = list(zip(*check_grid))[::-1]
                 rotated = [list(elem) for elem in rotated]
-                self._grid = rotated
+                check_grid = rotated
         else:
             i, j = cords
 
-        checked = checked if checked is not None else []
-
         while True:
-            next_checks = self.check_near_connection((i, j), player)
+            next_checks = self.check_near_connection((i, j), player, check_grid)
 
             if j == 0 and not next_checks:
                 i += 2
@@ -62,8 +69,8 @@ class Game:
                 checked.append((i, j))
                 for x in range(len(next_checks)):
                     if next_checks[x] not in checked:
-                        if (self.check_win_connection(player, next_checks[x],
-                            checked) is not None):
+                        if (self.check_win_connection(player, check_grid,
+                            next_checks[x], checked) is not None):
                             return player
                         else:
                             checked.append(next_checks[x])
@@ -82,6 +89,10 @@ class Game:
     pass
 
 if __name__ == "__main__":
-    game = Game()
-    print(game.check_win_connection("A"))
-    print(game.check_win_connection("B"))
+    game = Game(grid)
+    gameA = Game(gridA)
+    gameB = Game(gridB)
+    print(gameA.check_win_connection("A"))
+    print(gameB.check_win_connection("B"))
+
+    print(gameA.check_win())
