07a714609e9329a7c9997534df28c0ceaaa8e98d
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-10 01:37:50 +0100

    To i owo

--- gale.py
+++ gale.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     while stage != "exit":
         stage = interface.choose_mode(stage) if stage == "starting" else stage
         stage = interface.two_players(stage) if stage == "2_players" else stage
-        stage = interface.ai_easy_test(stage) if stage == "AI-Easy" else stage
+        stage = interface.ai_easy(stage) if stage == "AI-Easy" else stage
         # ai hard
         stage = interface.over(stage) if stage == "over" else stage
 
