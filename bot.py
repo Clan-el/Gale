773a49e6a85dc296b8825101108b260ae0019efd
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-13 15:01:40 +0100

    Ogólne usprawnienia

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ from grid import Grid
 
 def easy_bot_move(grid: Grid) -> tuple[int, int]:
     now = datetime.now()
-    seed(now.minute * now.second + now.microsecond)
+    seed(now.minute ** now.second - now.microsecond)
     return choice(grid.free_cells())
 
 
-def hard_bot_mode(grid: Grid):
+def hard_bot_mode(grid: Grid) -> tuple[int, int]:
     pass
