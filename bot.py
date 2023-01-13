39f77e53ae0024bb5b4d7071f1c088a025153e56
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-13 14:13:20 +0100

    Usprawniono i przyspieszono podejmowanie decyzji

--- bot.py
+++ bot.py
@@ -1,15 +1,13 @@
-from random import randint, seed
+from random import seed, choice
 from datetime import datetime
+from grid import Grid
 
 
-def easy_bot_move(grid: list[list[str | None]]) -> tuple[int, int]:
+def easy_bot_move(grid: Grid) -> tuple[int, int]:
     now = datetime.now()
-    seed(now.minute * now.second * now.microsecond)
-    while True:
-        row, column = randint(1, 11), randint(1, 11)
-        if grid[row][column] is None:
-            return (row, column)
+    seed(now.minute * now.second + now.microsecond)
+    return choice(grid.free_cells())
 
 
-def hard_bot_mode(game):
+def hard_bot_mode(grid: Grid):
     pass
