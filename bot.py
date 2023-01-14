e0d3adc4860a85f31f81a45fc961aa61508536df
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-14 17:43:21 +0100

    Przystosowano kod do nowych implementacji

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from random import seed, choice
 from datetime import datetime
 from grid import Grid
+from interface import Interface
 
 now = datetime.now()
 seed(now.minute ** now.second - now.microsecond)
@@ -9,39 +10,44 @@ def easy_bot_move(grid: Grid) -> tuple[int, int]:
     return choice(grid.free_cells())
 
 
-def hard_bot_move(game, game_copy):
-    available_moves = game.grid.free_cells()
+def hard_bot_move(grid: Grid, interface: Interface=None):
+    available_moves = grid.free_cells()
     best_move = None
     best_score = float('-inf')
-    simulations = 1600 - 9*game.size**2
+    simulations = 1600 - 9*grid.size**2
+
     for count, move in enumerate(available_moves):
         percent = ((count*100)/len(available_moves))
-        game.interface.change_caption(f"Gracz Niebieski główkuje: {percent:.2f}%")
-        game.grid.change_cell(move, game.player2)
-        score = monte_carlo_tree_search(game, game_copy, simulations)
-        game.grid.change_cell(move, None)
 
+        if interface is not None:
+            text = f"Gracz Niebieski główkuje: {percent:.2f}%"
+            interface.change_caption(text)
+
+        grid.change_cell(move, grid.player2)
+        score = monte_carlo_tree_search(grid, simulations)
+        grid.change_cell(move, None)
         if score > best_score:
             best_score = score
             best_move = move
     return best_move
 
-def monte_carlo_tree_search(game, game_copy, simulations):
+def monte_carlo_tree_search(grid: Grid, simulations):
     wins = 0
     for _ in range(simulations):
-        grid_copy = Grid(game.player1, game.player2, game.size)
-        grid_copy._set_grid([row[:] for row in game.grid.get_grid()])
-        winner = simulate_random_game(grid_copy, game_copy)
-        if winner == game.player2:
+        grid_copy = Grid(grid.player1, grid.player2, grid.size)
+        grid_copy.set_grid([row[:] for row in grid.get_grid()])
+        winner = simulate_random_game(grid_copy)
+        if winner == grid.player2:
             wins += 1
     return wins / simulations
 
-def simulate_random_game(grid: Grid, game_copy):
+def simulate_random_game(grid: Grid):
     current_player = grid.player1
-    game_copy.set_grid(grid)
-    while game_copy.check_win() is None and grid.free_cells():
+    while grid.check_win() is None and grid.free_cells():
         available_moves = grid.free_cells()
         move = choice(available_moves)
         grid.change_cell(move, current_player)
-        current_player = grid.player1 if current_player == grid.player2 else grid.player2
-    return game_copy.check_win()
+
+        condition = current_player == grid.player2
+        current_player = grid.player1 if condition else grid.player2
+    return grid.check_win()
