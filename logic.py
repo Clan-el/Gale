fc12fbf6562be698d69357f2eaff86b14a9a690a
Radoslaw Slepowronski <slepowronski.radek@gmail.com>
2023-01-15 18:43:57 +0100

    Zmieniono wywoływanie interfejsu i siatki

--- logic.py
+++ logic.py
@@ -7,18 +7,18 @@ class Game:
     def __init__(self):
         self.player1 = "C"
         self.player2 = "N"
-        self.size = 7  # musi być nieparzyste
-        self.grid = Grid(self.player1, self.player2, self.size)
 
     def run(self):
-        self.interface = Interface(self.grid)
+        self.interface = Interface()
         while True:
             mode = self.interface.choose_mode()
-            winner = self.play(mode)
+            size = self.interface.choose_size()
+            winner = self.play(mode, size)
             self.interface.over(winner)
 
-    def play(self, mode):
-        self.grid.clear_grid()
+    def play(self, mode, size):
+        self.grid = Grid(self.player1, self.player2, size)
+        self.interface.set_grid(self.grid)
         self.interface.clear_screen()
         self.interface.draw_board(self.interface.board)
 
@@ -34,7 +34,7 @@ class Game:
                 moves += 1
             return self.grid.check_win()
 
-        elif mode == "test":
+        elif mode == "AI-AI":
             self.grid.move = 0
             while True:
                 self.grid.move += 1
@@ -47,7 +47,7 @@ class Game:
                 if winner is not None:
                     return winner
 
-                self.interface.change_caption("Gracz Niebieski zastanawia się")
+                # self.interface.change_caption("Gracz Niebieski zastanawia się")
                 cell = hard_bot_move(self.grid, self.player2, self.interface)
                 self.grid.change_cell(cell, self.player2)
                 self.interface.draw_board(self.interface.board)
@@ -68,7 +68,7 @@ class Game:
                 if winner is not None:
                     return winner
 
-                self.interface.change_caption("Gracz Niebieski zastanawia się")
+                # self.interface.change_caption("Gracz Niebieski zastanawia się")
                 if mode == "AI-Easy":
                     sleep(0.7)
                     cell = easy_bot_move(self.grid)
